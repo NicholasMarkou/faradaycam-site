0e110d536f244958eb616c5269e8426835f04dbd
NicholasMarkou <nmarkou2000@gmail.com>
2021-11-23 17:45:44 -0500

    Fixed picture storing issue

---
+++ camera.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+cp /home/pi/website/public/curPicture.jpg "/home/pi/website/images/$(date +"%Y_%m_%d_%H_%M").jpg"
---
+++ takePicNight.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+raspistill -o /home/pi/website/public/curPicture.jpg -w 1600 -h 1200 -a 12 -ex night -t 5000
+cp /home/pi/website/public/curPicture.jpg "/home/pi/website/images/$(date +"%Y_%m_%d_%H_%M_%S").jpg"
+sleep 15
+raspistill -o /home/pi/website/public/curPicture.jpg -w 1600 -h 1200 -a 12 -ex night -t 5000
+cp /home/pi/website/public/curPicture.jpg "/home/pi/website/images/$(date +"%Y_%m_%d_%H_%M_%S").jpg"
+sleep 15
+raspistill -o /home/pi/website/public/curPicture.jpg -w 1600 -h 1200 -a 12 -ex night -t 5000
+cp /home/pi/website/public/curPicture.jpg "/home/pi/website/images/$(date +"%Y_%m_%d_%H_%M_%S").jpg"
