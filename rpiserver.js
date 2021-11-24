9c1fafc39b67100e2c07477c5be2bbc619bb1e04
NicholasMarkou <nmarkou2000@gmail.com>
2021-11-23 20:22:16 -0500

    Fixed sockets - output goes to panel now

--- rpiserver.js
+++ rpiserver.js
@@ -1,8 +1,10 @@
 const express = require( 'express' ),
       env = require('dotenv').config(),
+      bodyParser = require("body-parser"),
       cookie = require('cookie-session'),
       serveIndex = require('serve-index'),
       fs = require('fs'),
+      http = require('http'),
       spawn = require("child_process").spawn;
       
 const app = express();
@@ -13,6 +15,7 @@ const dir = process.env.DIREC+"/public/";
 app.use( express.urlencoded({ extended:true }) )
 app.use('/timelapse', serveIndex(dir+'/timelapse'));
 app.use('/images', serveIndex(dir + '/images'));
+app.use( bodyParser.json() )
 
 app.use(cookie({
     name: 'session',
@@ -39,26 +42,38 @@ app.post( '/login', (req,res)=> {
     }
 )
 
+io.on('connection', (socket) => {
+	console.log('connected');
+	
+	socket.on('disconnect', () => {
+		console.log('disconnected');
+	});
+})
+
+
 app.post('/adminCompileLapse', (req,res)=> {
     args=[
-	'-framerate', req.body.frameRate.toString(),
+	'-framerate', req.body.frameRate,
 	'-pattern_type','glob',
 	'-i','/home/pi/website/public/images/*.jpg',
 	'-c:v','libx264',
-  '-frames:v', (req.body.maxFrames === "" ? '180' : req.body.maxFrames),
+  	'-frames:v', (req.body.maxFrames === "" ? '180' : req.body.maxFrames),
 	'-crf','20','/home/pi/website/public/timelapse/'+ req.body.nameOfLapse +'.mp4']
 
     let cmd = spawn('ffmpeg', args);
-    io.on('connection', (socket) => {
-      console.log('connected');
-    });
+    io.emit('cmd', 'compiling');
     cmd.stdout.on('data', (data) => {
-      io.emit('cmd', data);
-    });
-    res.sendStatus(200);
+	io.emit('cmd', data);
+	});
+    cmd.stdout.on('close', (data) => {
+	io.emit('cmd', 'done compiling');
+	});
+    cmd.stderr.on('data', (data) => {
+	io.emit('cmd', data.toString());
+	});
 })
 
-app.use(express.static(dir));
+app.use(express.static("./public/"));
 const listener = server.listen(PORT, () => {
   console.log("Listening on port " + listener.address().port);
 });
